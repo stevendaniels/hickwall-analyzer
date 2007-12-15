be7741bb9bd0178a0d6cab67e1bc8d7a98890094
aminbu@gmail.com <aminbu@gmail.com@437fc3d2-a13f-0410-b837-9ff12401d523>
2007-12-15 04:04:26 +0000

    Add DictionaryTest

---
+++ Hickwall-Src/Source/main/test/com/novse/segmentation/core/matching/dictionary/DictionaryTest.java
@@ -0,0 +1,15 @@
+package com.novse.segmentation.core.matching.dictionary;
+
+import junit.framework.TestCase;
+
+public class DictionaryTest extends TestCase {
+
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+
+}
