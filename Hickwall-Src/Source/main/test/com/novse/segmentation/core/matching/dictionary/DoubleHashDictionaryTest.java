5e04eac3e5648d7d33aa1d2a1916b11a9abba170
aminbu@gmail.com <aminbu@gmail.com@437fc3d2-a13f-0410-b837-9ff12401d523>
2007-12-17 12:19:03 +0000

    初步添加了对各个词典类的测试

---
+++ Hickwall-Src/Source/main/test/com/novse/segmentation/core/matching/dictionary/DoubleHashDictionaryTest.java
@@ -0,0 +1,15 @@
+package com.novse.segmentation.core.matching.dictionary;
+/**
+ * ”√”⁄≤‚ ‘DoubleHashDictionary
+ * 
+ * @author gbu
+ *
+ */
+public class DoubleHashDictionaryTest extends DictionaryTest {
+
+	@Override
+	protected Dictionary getDictionary() {
+		return new DoubleHashDictionary();
+	}
+
+}
---
+++ Hickwall-Src/Source/main/test/com/novse/segmentation/core/matching/dictionary/HashDictionaryTest.java
@@ -0,0 +1,16 @@
+package com.novse.segmentation.core.matching.dictionary;
+
+/**
+ * ”√”⁄≤‚ ‘HashDictionary.
+ * 
+ * @author gbu
+ *
+ */
+public class HashDictionaryTest extends DictionaryTest {
+
+	@Override
+	protected Dictionary getDictionary() {
+		return new HashDictionary();
+	}
+
+}
