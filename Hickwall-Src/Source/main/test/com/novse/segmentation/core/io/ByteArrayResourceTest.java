13005ad1f2d77d3ad8b61e1e62aa6ff3ea8b02bd
billgmh <billgmh@437fc3d2-a13f-0410-b837-9ff12401d523>
2008-01-12 09:54:45 +0000

    修改词典载入模式，优化词典批量添加

--- Hickwall-Src/Source/main/test/com/novse/segmentation/core/io/ByteArrayResourceTest.java
+++ Hickwall-Src/Source/main/test/com/novse/segmentation/core/io/ByteArrayResourceTest.java
@@ -11,9 +11,6 @@ import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 
-import com.novse.segmentation.core.io.ByteArrayResource;
-import com.novse.segmentation.core.io.Resource;
-
 /**
  * 
  * @author Mac Kwan 字节数组资源单元测试
@@ -27,31 +24,30 @@ public class ByteArrayResourceTest extends ResourceTest
         byte[] result = null;
         try
         {
-            //输入流
+            // 输入流
             BufferedInputStream in = new BufferedInputStream(
                     new FileInputStream(new File(this.getClass().getResource(
                             "/file/ResourceTest.txt").getFile())));
-            //输出流
+            // 输出流
             ByteArrayOutputStream out = new ByteArrayOutputStream();
-            //缓冲
+            // 缓冲
             byte[] buffer = new byte[1024];
-            //读入多少字节
+            // 读入多少字节
             int i;
-            while((i=in.read(buffer))>0)
+            while ((i = in.read(buffer)) > 0)
             {
                 out.write(buffer, 0, i);
             }
             out.flush();
-            
-            //转换为字节数组
-            result=out.toByteArray();
-            
+
+            // 转换为字节数组
+            result = out.toByteArray();
+
             out.close();
             in.close();
         }
         catch (Exception e)
         {
-            // TODO 自动生成 catch 块
             e.printStackTrace();
         }
 
