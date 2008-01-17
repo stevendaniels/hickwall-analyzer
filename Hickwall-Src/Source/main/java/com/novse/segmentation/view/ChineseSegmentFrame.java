8a1607952928f4b5d6b6872207cf4be5866e3d61
billgmh <billgmh@437fc3d2-a13f-0410-b837-9ff12401d523>
2008-01-17 15:19:17 +0000

    加入版权信息

--- Hickwall-Src/Source/main/java/com/novse/segmentation/view/ChineseSegmentFrame.java
+++ Hickwall-Src/Source/main/java/com/novse/segmentation/view/ChineseSegmentFrame.java
@@ -1,3 +1,19 @@
+/* 
+ * Copyright hickwall 
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not 
+ * use this file except in compliance with the License. You may obtain a copy 
+ * of the License at 
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0 
+ *   
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT 
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the 
+ * License for the specific language governing permissions and limitations 
+ * under the License.
+ * 
+ */
 package com.novse.segmentation.view;
 
 import java.awt.BorderLayout;
@@ -45,11 +61,10 @@ import com.novse.segmentation.core.unlistedword.dictionary.ChineseNameDictionary
 import com.novse.segmentation.core.unlistedword.dictionary.NotChineseNameDictionary;
 import com.novse.segmentation.core.unlistedword.name.SimpleChineseNameAnalyzer;
 import com.novse.segmentation.lucene.analysis.query.HickwallQueryAnalyzer;
-import com.novse.segmentation.util.DictionaryUtil;
 
 /*
  * @作者:Hades , 创建日期:Apr 18, 2007
- *
+ * 
  * 汕头大学03计算机本科
  * 
  */
@@ -653,12 +668,12 @@ public class ChineseSegmentFrame extends JFrame
             {
                 public void actionPerformed(ActionEvent e)
                 {
-                    if (nameDic != null)
-                        new DictionaryUtil<ChineseNameDictionary>()
-                                .writeDictionary(nameDic, "Dic/Name.stu");
-                    if (notNameDic != null)
-                        new DictionaryUtil<NotChineseNameDictionary>()
-                                .writeDictionary(notNameDic, "Dic/UnName.stu");
+                    // if (nameDic != null)
+                    // new DictionaryUtil<ChineseNameDictionary>()
+                    // .writeDictionary(nameDic, "Dic/Name.stu");
+                    // if (notNameDic != null)
+                    // new DictionaryUtil<NotChineseNameDictionary>()
+                    // .writeDictionary(notNameDic, "Dic/UnName.stu");
                     System.exit(0);
                 }
             });
