ab09967e446c76898c1c1801a4aed1488d49214d
billgmh <billgmh@437fc3d2-a13f-0410-b837-9ff12401d523>
2008-05-18 04:30:06 +0000

    Dictionary Unit Test优化

--- Hickwall-Src/Source/main/test/com/novse/segmentation/core/matching/dictionary/DiagramDictionaryTest.java
+++ Hickwall-Src/Source/main/test/com/novse/segmentation/core/matching/dictionary/DiagramDictionaryTest.java
@@ -4,13 +4,15 @@ package com.novse.segmentation.core.matching.dictionary;
  * ”√”⁄≤‚ ‘DiagramDictionary
  * 
  * @author gbu
- *
+ * 
  */
-public class DiagramDictionaryTest extends DictionaryTest {
+public class DiagramDictionaryTest extends DictionaryTest
+{
 
-	@Override
-	protected Dictionary getDictionary() {
-		return new DiagramDictionary();
-	}
+    @Override
+    protected Dictionary getDictionary()
+    {
+        return new DiagramDictionary();
+    }
 
 }
--- Hickwall-Src/Source/main/test/com/novse/segmentation/core/matching/dictionary/SimpleDictionaryTest.java
+++ Hickwall-Src/Source/main/test/com/novse/segmentation/core/matching/dictionary/SimpleDictionaryTest.java
@@ -4,14 +4,15 @@ package com.novse.segmentation.core.matching.dictionary;
  * ”√”⁄≤‚ ‘SimpleDictionary
  * 
  * @author gbu
- *
+ * 
  */
-public class SimpleDictionaryTest extends DictionaryTest {
+public class SimpleDictionaryTest extends DictionaryTest
+{
 
-	@Override
-	protected Dictionary getDictionary() {
-		return new SimpleDictionary();
-	}
-	
-	
+    @Override
+    protected Dictionary getDictionary()
+    {
+        return new SimpleDictionary();
+    }
+
 }
